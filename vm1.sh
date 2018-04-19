d5e1ea1a4eab72ff6f9900388eac9b1f261a525a
asbilyk <asbilyk@gmail.com>
2018-04-20 00:01:50 +0300

    just tried

--- vm1.sh
+++ vm1.sh
@@ -2,30 +2,43 @@
 
 source vm1.config
 export $(cut -d= -f1 vm1.config)
-envsubst < default '$HTTP_SERV_IP', '$HTTPS_PORT' > /etc/nginx/site-enabled/default
-
 
 modprobe 8021q
-vconfig add $INT_IF $VLAN_ID
-ip addr add $VLAN_IP dev $INT_IF:$VLAN_ID
-ip link set up $INT_IF:$VLAN_ID
+vconfig add $INTERNAL_IF $VLAN
+ip addr add $VLAN_IP dev $INTERNAL_IF:$VLAN
+ip link set up $INTERNAL_IF:$VLAN
 
 sysctl net.ipv4.ip_forward=1
 
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
+ifup $INTERNAL_IF
+ifup $EXTERNAL_IF
 
-iptables -t nat -A POSTROUTING -o $EXT_IF -j MASQUERADE
-ip addr add $EXT_IP dev $EXT_IF
-ip link set up $EXT_IF
+iptables -t nat -A POSTROUTING -o $EXTERNAL_IF -j MASQUERADE
+ip addr add $EXT_IP dev $EXTERNAL_IF
+ip link set up $EXTERNAL_IF
 ip route add default via $GW_IP
 
-apt-get install nginx -y -q
+IP=$(ifconfig $EXTERNAL_IF|grep 'inet addr'|cut -d: -f2|awk '{print $1}')
+apt-get update && apt-get install nginx -y -q
 
+echo "
+server {
+        listen $IP:$NGINX_PORT ssl;
+	ssl on;
+	server_name $(hostname);
+	ssl_certificate /etc/ssl/certs/root-ca.crt;
+	ssl_certificate_key /etc/ssl/private/web.key;	 
+	location / {
+		proxy_pass http://$APACHE_VLAN_IP;	
+
+	}
+}" > /etc/nginx/site-available/default
 mkdir -p /etc/ssl/certs
 openssl genrsa -out /etc/ssl/certs/root-ca.key 4096
-openssl req -x509 -new -nodes -key /etc/ssl/certs/root-ca.key -sha256 -days 365 -out /etc/ssl/certs/root-ca.crt -subj "/C=UA/ST=Kharkiv/L=Kharkiv/CN=vm1/"
+openssl req -x509 -new -nodes -key /etc/ssl/certs/root-ca.key -sha256 -days 365 -out /etc/ssl/certs/root-ca.crt -subj "/C=UA/ST=Kharkiv/L=Kharkiv/OU=Mirantis/CN=vm1/"
 openssl genrsa -out /etc/ssl/certs/web.key 2048
-openssl req -new -out /etc/ssl/certs/web.csr -key /etc/ssl/certs/web.key -subj "/C=UA/ST=Kharkiv/L=Kharkiv/CN=vm1/"
+openssl req -new -out /etc/ssl/certs/web.csr -key /etc/ssl/certs/web.key -subj "/C=UA/ST=Kharkiv/L=Kharkiv/OU=Mirantis/CN=vm1/"
 openssl req -x509 -in  /etc/ssl/certs/web.csr -CA /etc/ssl/certs/root-ca.crt -CAkey /etc/ssl/certs/root-ca.key -CAcreateserial -out /etc/ssl/certs/web.crt
 
 cat /etc/ssl/certs/root-ca.crt /etc/ssl/certs/web.crt > /etc/ssl/certs/web-ca-cert
--- vm2.sh
+++ vm2.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
-apt-get update && apt-get install apache2 -y -q
 
 source vm2.config
 export $(cut -d= -f1 vm2.config)
-envsubst < 000-default.conf 'HTTP_SERV_IP'> /etc/apache2/sites-enabled/000-default.conf
 
 modprobe 8021q
-vconfig add $INT_IF $VLAN_ID
-ip addr add $HTTP_SERV_IP dev $INT_IF:$VLAN_ID
-ip link set up $INT_IF:$VLAN_ID
+vconfig add $INTERNAL_IF $VLAN
+ip addr add $APACHE_VLAN_IP dev $INTERNAL_IF:$VLAN
+ip link set up $INTERNAL_IF:$VLAN
 ip route add default via $GW_IP
+apt-get update && apt-get install apache2 -y -q $1>/dev/null
+echo "
+<VirtualHost *:80>
+    ServerName $(hostname)
+    DocumentRoot /var/www/html
+<VirtualHost>" > /etc/apache2/sites-enable/000-default.conf
+
+service apache2 restart
 
